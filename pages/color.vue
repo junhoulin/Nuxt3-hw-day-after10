ec2a9c825e42370b488b4439f4d207791e23d305
JUNHOULIN <yy6313531@gmail.com>
2024-12-03 11:27:15 +0800

    update day18

---
+++ pages/color.vue
@@ -0,0 +1,15 @@
+<script setup>
+const pureColor = ref("red");
+const gradientColor = ref(
+  "linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 100%)"
+);
+</script>
+
+<template>
+  <ClientOnly>
+    <ColorPicker
+      v-model:pureColor="pureColor"
+      v-model:gradientColor="gradientColor"
+    />
+  </ClientOnly>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+const isLoading = ref(false);
+function openLoading() {
+  // 開啟讀取效果
+  isLoading.value = true;
+  setTimeout(() => {
+    // 關閉讀取效果
+    isLoading.value = false;
+  }, 2000);
+}
+</script>
+
+<template>
+  <ClientOnly>
+    <loading v-model:active="isLoading"
+          :can-cancel="true"
+          :on-cancel="onCancel"
+          :is-full-page="fullPage"/>
+  </ClientOnly>
+  <button type="button" @click="openLoading">開啟 Loading 效果</button>
+</template>
+
+<style scoped></style>
